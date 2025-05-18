e9446ff4490e4457f346d084149bf3f9a67e1151
Mattia Marini <marmattia26@gmail.com>
2025-05-18 03:09:49 +0200

    Added first deck display draft

--- src/workspace/Workspace.tsx
+++ src/workspace/Workspace.tsx
@@ -6,11 +6,31 @@ import rehypeRaw from "rehype-raw";
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 
+type MarkdownInfos = {
+  title: string;
+  id: number | null;
+  front: string;
+  back: string | null;
+};
+
+type JsonInfos = {
+  creation_timestamp: string; // ISO 8601
+  last_review: string | null;
+  scores: number[];
+};
+
+type Flashcard = {
+  from_markdown: MarkdownInfos;
+  from_json: JsonInfos;
+};
+
 function Workspace({ workspacePath }: { workspacePath: string[] | null }) {
-  let [markdown, setMarkdown] = useState<string>("");
+  let [parsedFlashcards, setParsedFlashcards] = useState<Record<string, Flashcard> | null>(null);
+  let [currCardIndex, setCurrCardIndex] = useState(0);
   let [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
+
 
     if (workspacePath && workspacePath.length === 2) {
       let jsonFile = null;
@@ -26,7 +46,7 @@ function Workspace({ workspacePath }: { workspacePath: string[] | null }) {
 
       if (jsonFile && markdownFile) {
         invoke("read_markdown", { markdown: markdownFile, json: jsonFile }).then((data) => {
-          setMarkdown(data as string);
+          setParsedFlashcards(JSON.parse(data as string));
         })
       } else {
         setError("Please select a markdown file and a json file");
@@ -54,7 +74,15 @@ function Workspace({ workspacePath }: { workspacePath: string[] | null }) {
         {error && <div className={styles.error}>{error}</div>}
         <div className={`${styles.markdownContainer} ${styles.invisibleScrollbar}`}>
           <div >
-            <Markdown rehypePlugins={[rehypeRaw]} >{markdown}</Markdown>
+            <Markdown rehypePlugins={[rehypeRaw]}>
+              {
+                parsedFlashcards
+                  ? (Object.keys(parsedFlashcards).length > 0
+                    ? parsedFlashcards[Object.keys(parsedFlashcards)[currCardIndex]].from_markdown.front
+                    : '# Empty deck!')
+                  : 'No deck loaded'
+              }
+            </Markdown>
           </div>
         </div>
       </div>
